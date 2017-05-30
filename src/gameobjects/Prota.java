de7b331237e8cab6e887995812013c046457135d
dam1a <dam1a@Aulaxx-yy>
2017-05-30 17:31:35 +0200

    La clase prota sin terminar

---
+++ src/gameobjects/Prota.java
@@ -0,0 +1,73 @@
+package gameobjects;
+
+public class Prota {
+
+    private int vida;
+    private int danyo;
+    private int y;
+    private int x;
+    private String sprite;
+
+    public Prota(int vida, int danyo, int y, int x, String sprite) {
+        this.vida = vida;
+        this.danyo = danyo;
+        this.y = y;
+        this.x = x;
+        this.sprite = sprite;
+    }
+
+    public int getVida() {
+        return vida;
+    }
+
+    public void setVida(int vida) {
+        this.vida = vida;
+    }
+
+    public int getDanyo() {
+        return danyo;
+    }
+
+    public void setDanyo(int danyo) {
+        this.danyo = danyo;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public String getSprite() {
+        return sprite;
+    }
+
+    public void setSprite(String sprite) {
+        this.sprite = sprite;
+    }
+
+    public void aumentarDanyo(int arma) {
+        this.danyo = this.danyo + arma;
+    }
+
+//    public void restaurarVida() {
+//        int vidaActual;
+//        int vidaMax = 100;
+//        vidaActual = getVida();
+//        if (vidaActual < 100) {
+//            vidaMax - vidaActual;
+//        } else {
+//
+//        }
+//    }
+}
