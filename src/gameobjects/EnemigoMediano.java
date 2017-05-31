6652719e2f9334c4a82e41335746ff97f43cf333
MSI <MSI@192.168.1.170>
2017-05-30 17:39:40 -0700

    Clase de enemigo mediano sin terminar..

---
+++ src/gameobjects/EnemigoMediano.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gameobjects;
+
+/**
+ *
+ * @author MSI
+ */
+public class EnemigoMediano extends Malo {
+    
+    public EnemigoMediano(int vida, int resistencia, int danyo, int x, int y, String sprite) {
+        super(vida, resistencia, danyo, x, y, sprite);
+    }
+   
+    
+}
