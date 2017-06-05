f63e86d901a9d9905d37de1a6b4aa7ee093623f5
elcampos85 <elcampos85@gmail.com>
2017-06-05 18:09:29 +0200

    Crear clase mapa

--- src/gameobjects/Mapa.java
+++ src/gameobjects/Mapa.java
@@ -17,6 +17,22 @@ public class Mapa {
         this.x = x;
         this.y = y;
     }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
     
     
 }
